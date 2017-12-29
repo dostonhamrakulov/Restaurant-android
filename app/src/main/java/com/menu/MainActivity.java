c23cbd43de257868897c18703c4f73864c8745d0
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2017-12-29 04:52:01 +0200

    Changes in String.xml

--- app/src/main/java/com/menu/MainActivity.java
+++ app/src/main/java/com/menu/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
